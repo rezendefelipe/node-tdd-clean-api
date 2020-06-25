fc34b4188901dd14004d18c3f713063b554cfa36
felipe rezende <felipevrezende@hotmail.com>
2020-06-25 15:22:52 -0300

    ensure email on email validator

---
+++ src/utils/email-validation.spec.js
@@ -0,0 +1,31 @@
+const validator = require('validator')
+class EmailValidator {
+  isValid (email) {
+    return validator.isEmail(email)
+  }
+}
+
+const makeSut = () => {
+  return new EmailValidator()
+}
+
+describe('Email Validator', () => {
+  test('Should return true if validator returns true', async () => {
+    const sut = makeSut()
+    const isEmailValid = sut.isValid('valid_email@email.com')
+    expect(isEmailValid).toBe(true)
+  })
+
+  test('Should return false if validator returns false', async () => {
+    validator.isEmailValid = false
+    const sut = makeSut()
+    const isEmailValid = sut.isValid('valid_email@email.com')
+    expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validator with correct email', async () => {
+    const sut = makeSut()
+    sut.isValid('any_email@email.com')
+    expect(validator.email).toEqual('any_email@email.com')
+  })
+})
