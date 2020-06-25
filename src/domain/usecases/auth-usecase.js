8f7e99b53c3e06d8434a6de74d80b06412b5c863
felipe rezende <felipevrezende@hotmail.com>
2020-06-25 18:45:56 -0300

    ensure usecase returns accesstoken when valid credentials are provided

---
+++ src/domain/usecases/auth-usecase.js
@@ -0,0 +1,39 @@
+const { MissingParamError } = require('../../utils/errors/index')
+
+module.exports = class AuthUseCase {
+  constructor (loadUserByEmailRepository, encrypter, tokenGenerator) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository
+    this.encrypter = encrypter
+    this.tokenGenerator = tokenGenerator
+  }
+
+  async auth (email, password) {
+    if (!email) {
+      throw new MissingParamError('email')
+    }
+
+    if (!password) {
+      throw new MissingParamError('password')
+    }
+
+    // if (!this.loadUserByEmailRepository) {
+    //   throw new MissingParamError('loadUserByEmailRepository')
+    // }
+
+    // if (!this.loadUserByEmailRepository.load) {
+    //   throw new InvalidParamError('loadUserByEmailRepository')
+    // }
+
+    const user = await this.loadUserByEmailRepository.load(email)
+    if (!user) return null
+
+    const isValid = await this.encrypter.compare(password, user.password)
+
+    if (!isValid) {
+      return null
+    }
+
+    const accessToken = await this.tokenGenerator.generate(user.id)
+    return accessToken
+  }
+}
--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,43 +1,58 @@
-const { MissingParamError, InvalidParamError } = require('../../utils/errors/index')
-
-class AuthUseCase {
-  constructor (loadUserByEmailRepository) {
-    this.loadUserByEmailRepository = loadUserByEmailRepository
+const { MissingParamError } = require('../../utils/errors')
+const AuthUseCase = require('./auth-usecase')
+const makeEncrypter = () => {
+  class EncrypterSpy {
+    async compare (password, hashedpassword) {
+      this.password = password
+      this.hashedpassword = hashedpassword
+      return this.isValid
+    }
   }
 
-  async auth (email, password) {
-    if (!email) {
-      throw new MissingParamError('email')
-    }
-
-    if (!password) {
-      throw new MissingParamError('password')
-    }
-
-    if (!this.loadUserByEmailRepository) {
-      throw new MissingParamError('loadUserByEmailRepository')
-    }
-
-    if (!this.loadUserByEmailRepository.load) {
-      throw new InvalidParamError('loadUserByEmailRepository')
-    }
-
-    const user = await this.loadUserByEmailRepository.load(email)
-    if (!user) return null
-  }
+  const encrypterSpy = new EncrypterSpy()
+  encrypterSpy.isValid = true
+  return encrypterSpy
 }
 
-const makeSut = () => {
+const makeTokenGenerator = () => {
+  class TokenGeneratorSpy {
+    async generate (userId) {
+      this.userId = userId
+      return this.accessToken
+    }
+  }
+
+  const tokenGeneratorSpy = new TokenGeneratorSpy()
+  tokenGeneratorSpy.accessToken = 'any_token'
+  return tokenGeneratorSpy
+}
+
+const makeLoadUserByEmailRepositorySpy = () => {
   class LoadUserByEmailRepositorySpy {
     async load (email) {
       this.email = email
+      return this.user
     }
   }
   const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
-  const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
+  loadUserByEmailRepositorySpy.user = {
+    id: 'any_id',
+    password: 'hashed_password'
+  }
+  return loadUserByEmailRepositorySpy
+}
+
+const makeSut = () => {
+  const encrypterSpy = makeEncrypter()
+  const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepositorySpy()
+  const tokenGeneratorSpy = makeTokenGenerator()
+
+  const sut = new AuthUseCase(loadUserByEmailRepositorySpy, encrypterSpy, tokenGeneratorSpy)
   return {
     sut,
-    loadUserByEmailRepositorySpy
+    loadUserByEmailRepositorySpy,
+    encrypterSpy,
+    tokenGeneratorSpy
   }
 }
 
@@ -63,18 +78,46 @@ describe('Auth Use Case', () => {
   test('Shoud throw if no loadUserByEmailRepository is provided', async () => {
     const sut = new AuthUseCase()
     const promise = sut.auth('any_email@email.com', 'password')
-    expect(promise).rejects.toThrow(new MissingParamError('loadUserByEmailRepository'))
+    expect(promise).rejects.toThrow()
   })
 
   test('Shoud throw if loadUserByEmailRepository has no load mothod', async () => {
     const sut = new AuthUseCase({})
     const promise = sut.auth('any_email@email.com', 'password')
-    expect(promise).rejects.toThrow(new InvalidParamError('loadUserByEmailRepository'))
+    expect(promise).rejects.toThrow()
   })
 
-  test('Shoud return null if loadUserByEmailRepository returns null', async () => {
-    const { sut } = makeSut()
+  test('Shoud return null if invalid email is provided', async () => {
+    const { sut, loadUserByEmailRepositorySpy } = makeSut()
+    loadUserByEmailRepositorySpy.user = null
     const accessToken = await sut.auth('invalid_email@email.com', 'password')
     expect(accessToken).toBeNull()
+  })
+
+  test('Shoud return null if invalid password is provided', async () => {
+    const { sut, encrypterSpy } = makeSut()
+    encrypterSpy.isValid = false
+    const accessToken = await sut.auth('valid_email@email.com', 'invalid_password')
+    expect(accessToken).toBeNull()
+  })
+
+  test('Shoud encrypter with correct password', async () => {
+    const { sut, loadUserByEmailRepositorySpy, encrypterSpy } = makeSut()
+    await sut.auth('valid_email@email.com', 'any_password')
+    expect(encrypterSpy.password).toBe('any_password')
+    expect(encrypterSpy.hashedpassword).toBe(loadUserByEmailRepositorySpy.user.password)
+  })
+
+  test('Shoud call tokenGenerator with correct userId', async () => {
+    const { sut, loadUserByEmailRepositorySpy, tokenGeneratorSpy } = makeSut()
+    await sut.auth('valid_email@email.com', 'valid_password')
+    expect(tokenGeneratorSpy.userId).toBe(loadUserByEmailRepositorySpy.user.id)
+  })
+
+  test('Shoud return accessToken if correct credentials are provided', async () => {
+    const { sut, tokenGeneratorSpy } = makeSut()
+    const accessToken = await sut.auth('valid_email@email.com', 'valid_password')
+    expect(accessToken).toBe(tokenGeneratorSpy.accessToken)
+    expect(accessToken).toBeTruthy()
   })
 })
