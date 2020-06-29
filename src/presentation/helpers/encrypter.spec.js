7111579593e1c7a158f5f4a80509d13d425d386b
felipe rezende <felipevrezende@hotmail.com>
2020-06-29 15:21:38 -0300

    encrypter spec file tests

---
+++ src/presentation/helpers/encrypter.spec.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcrypt')
+const Encrypter = require('./encrypter')
+const { MissingParamError } = require('../../utils/errors')
+
+const makeSut = () => {
+  return new Encrypter()
+}
+
+describe('Encrypter Validator', () => {
+  test('Should return true if bcrypt returns true', async () => {
+    const sut = makeSut()
+    const isValid = await sut.compare('any_value', 'hashed_value')
+    expect(isValid).toBe(true)
+  })
+
+  test('Should return false if bcrypt returns false', async () => {
+    const sut = makeSut()
+    bcrypt.isValid = false
+    const isValid = await sut.compare('any_value', 'hashed_value')
+    expect(isValid).toBe(false)
+  })
+
+  test('Should call bcrypt with corretc values', async () => {
+    const sut = makeSut()
+    await sut.compare('any_value', 'hashed_value')
+    expect(bcrypt.value).toBe('any_value')
+    expect(bcrypt.hash).toBe('hashed_value')
+  })
+
+  test('Should throw if no params are provided', async () => {
+    const sut = makeSut()
+    expect(sut.compare()).rejects.toThrow(new MissingParamError('value'))
+    expect(sut.compare('value')).rejects.toThrow(new MissingParamError('hash'))
+  })
+})
